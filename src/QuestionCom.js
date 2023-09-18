f59ba28804ea4fb01acd7e0f55e5760a474e1f3f
VaibhavGupta911 <guptaayush519@gmail.com>
2023-09-18 22:29:41 +0530

    changes

--- src/QuestionCom.js
+++ src/QuestionCom.js
@@ -4,13 +4,13 @@ export default function QuestionCom({ question, answer }) {
     const [isshowing, setIsdhowing] = useState(false);
     return (
         <div>
-            <article className="article-question">
-                <article onClick={() => setIsdhowing(!isshowing)}>
+            <article className="article-question" onClick={() => setIsdhowing(!isshowing)}>
+                <article >
                     <h2 className={`${isshowing && "clicked"}`}>
                         {question}
                     </h2>
+                    <div>{isshowing ? <FaChevronUp color='hsl(14, 88%, 65%)' /> : <FaChevronDown color='hsl(14, 88%, 65%)' />}</div>
 
-                    {isshowing ? <FaChevronUp color='hsl(14, 88%, 65%)' /> : <FaChevronDown color='hsl(14, 88%, 65%)' />}
                 </article>
                 {isshowing ? <p className="answer-p">{answer}</p> : null}
             </article>
